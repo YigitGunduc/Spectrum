e0657026e970bc7c5d610cf5853279d7ed61a923
YigitGunduc <yy347112@gmail.com>
2020-12-19 18:31:57 +0300

    input form and redirect works

--- WebApp/routes.py
+++ WebApp/routes.py
@@ -1,11 +1,28 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, redirect, url_for, render_template, request
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-   return render_template("index.html") 
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run() 
+@app.route("/", methods=["POST", "GET"])
+def index():
+    if request.method == "POST":
+        seed = request.form["nm"]
+        generatedText = seed
+        if len(seed) != 0:
+        	return redirect(f"/generate/{generatedText}")
+        else:
+            return redirect("/generate/please enter a valid seed")
+    else:
+        return render_template("index.html")
+
+ 
+@app.route('/generate/<lyrics>')
+def generate(lyrics):
+    return f'{lyrics}'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
