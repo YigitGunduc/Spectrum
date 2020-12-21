5f32e0fea20d5aed3f906cde0903f16f18c7bce4
YigitGunduc <yy347112@gmail.com>
2020-12-21 17:58:05 +0300

    fully functioning web-app and API

--- WebApp/routes.py
+++ WebApp/routes.py
@@ -1,4 +1,15 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, jsonify
+from generate import *
+import string
+import time as t
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from   tensorflow.keras.models import load_model
+from   tensorflow.keras.models import Sequential
+from   tensorflow.keras.losses import sparse_categorical_crossentropy
+from   tensorflow.keras.layers import LSTM,Dense,Embedding,Dropout,GRU
+
 
 app = Flask(__name__)
 
@@ -6,7 +17,7 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
         seed = request.form["nm"]
-        generatedText = seed
+        generatedText = generate_text(model, seed,gen_size=700,temp=1.0)
         if len(seed) != 0:
         	return redirect(f"/generate/{generatedText}")
         else:
@@ -17,9 +28,12 @@ def index():
 
 @app.route('/generate/<lyrics>')
 def generate(lyrics):
-	arr = lyrics.split(' ')
-	return render_template('result.html',lyrics=arr[0])
+    return render_template('result.html',lyrics=lyrics)
 
+@app.route('/api/generate/<seed>')
+def api(seed):
+    generatedText = generate_text(model, seed,gen_size=700,temp=1.0)
+    return jsonify({'lyrics' : generatedText})
 
 if __name__ == "__main__":
     app.run(debug=True)
