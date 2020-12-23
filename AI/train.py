f63b75a8c4cfa6e757897e65a7d860b8998b6070
YigitGunduc <yy347112@gmail.com>
2020-12-23 11:58:51 +0300

    train.py improved with pep 8 standards and started to use the new model structure

--- AI/train.py
+++ AI/train.py
@@ -8,16 +8,13 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM,Dense,Embedding,Dropout,GRU
 from tensorflow.keras.losses import sparse_categorical_crossentropy
 from tensorflow.keras.models import load_model
-from model import model
+from model import Generator
 
-# text dataset path
-path_to_file = 'raplyrics.txt'
+path_to_file = 'raplyrics.txt' # text dataset path
 
-# loading text dataset
-text = open(path_to_file, 'r').read()
+text = open(path_to_file, 'r').read() # loading text dataset
 
-# removing non ascii characters
-text = re.sub(r'[^\x00-\x7f]',r'', text) 
+text = re.sub(r'[^\x00-\x7f]',r'', text) # removing non ascii characters
 
 # removing escape characters
 text = text.replace('\x10', ' ') 
@@ -32,21 +29,25 @@ text = text.replace('\x02', ' ')
 text = text.replace('\x0e', ' ') 
 
 # constants variables
+# ------------------------------------------------------------
 vocab = sorted(set(string.printable)) # variety of characters
-vocab_size = len(vocab) # num of items in the vocab
-embed_dim = 64 # embeding dim size
-rnn_neurons = 1026 # number of neurans of out rnn units
-
-batch_size = 128 # batch_size
-buffer_size = 10000 # buffer_size
-epochs = 30 # num of epochs to train 
-seq_len = 120 # seq_len
-
-char_to_ind = {u:i for i, u in enumerate(vocab)}
+vocab_size = len(vocab) # num of items in the vocab          |
+embed_dim = 64 # embeding dim size                           
+rnn_neurons = 1026 # number of neurans of out rnn units      
+                                                             
+batch_size = 128 # batch_size                                
+buffer_size = 10000 # buffer_size                            
+epochs = 30 # num of epochs to train                         
+seq_len = 120 # seq_len                                      
+                                                 
+char_to_ind = {u:i for i, u in enumerate(vocab)}             
 ind_to_char = np.array(vocab)
 encoded_text = np.array([char_to_ind[c] for c in text])
+# ------------------------------------------------------------
 
- 
+
+# preprocessing the data 
+# ------------------------------------------------------------
 total_num_seq = len(text)//(seq_len+1)
 
 char_dataset = tf.data.Dataset.from_tensor_slices(encoded_text)
@@ -58,23 +59,15 @@ def create_seq_targets(seq):
     target_txt = seq[1:]
     return input_txt, target_txt
 
-
 dataset = sequences.map(create_seq_targets)
 
 dataset = dataset.shuffle(buffer_size).batch(batch_size, drop_remainder=True)
+# ------------------------------------------------------------
 
-model = model(
-  vocab_size = vocab_size,
-  embed_dim=embed_dim,
-  batch_size=batch_size)
+# Training the model
+# ------------------------------------------------------------
+model = Generator() # creating an instance of model
 
-print(model.summary())
-
-for i in range(epochs):
-  print(f'{i}/{epochs}')
-  model.fit(dataset,epochs=1)
-  if i % 3 ==0:
-    model.save(f'model-{i}-epochs.h5') 
-    print('model has been saved')
-
-model.save(f'model-{epochs}-epochs.h5') 
+# training the model
+model.train(dataset,epochs = 5, verbose=1, save_at=1)
+# ------------------------------------------------------------
